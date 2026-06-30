d6eec96c270efecec2bdb51a921cde41cf322cb7
malaikaahsan <amz.va.malaika@gmail.com>
2026-06-30 22:54:30 +0500

    Updated Url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "http://premium-car-rental-production.up.railway.app",
 });
 
 export default api;
